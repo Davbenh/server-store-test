6d9107cb2f10c1927731247c54f69201dd44b0e5
dudu benh <dav_bh@outlook.com>
2022-12-10 20:24:40 +0200

    123

--- BL/product.service.js
+++ BL/product.service.js
@@ -65,13 +65,9 @@ let product = [{
     inStock: 33
 }]
 
-<<<<<<< HEAD
 module.exports = {
     createNewProduct,
     updateProduct,
     
 
 }
-=======
-// init();
->>>>>>> c590f65f07a2dda7b3658fa8b006164fc6eaddd6
--- index.js
+++ index.js
@@ -1,31 +1,4 @@
-<<<<<<< HEAD
-require('dotenv').config() // חיבור קובץ ENV
-const express = require('express');//קורא לספרייה אקספרס
-const 
 
-const app = express();// קורא לפונקציית ברירת מחדל באקספרס ושם אותה בAPP
-
-const PORT = process.env.PORT || 3625;
-
-require("./DL/db").connect(); // קריאה וחיבור לבסיס נתונים
-
-//CORS
-
-const cors = require("cors"); // להריץ npm i cors -  פקודות אלה ממירים את הבקשות והתשובות לגייסון כדי שיוכלו לקרוא אותן.
-app.use(cors());
-app.use(express.json());
-
-
-app.get('/somepage', (req, res) => {     // פה אמרנו שאם מבצעים בקשה מסוג GET לכתובת הזאת - הפונקציה תתבצע.
-        console.log(req);
-        res.send("ok");
-})
-
-// , זו הפקודה שמריצה את השרת ואומרת לו להתחיל להקשיב , יש פורטים ששמורים
-app.listen(PORT, ()=>{
-    console.log('CONNECTION SUCCESSFUL: server is listening!!!');
-});
-=======
 require('dotenv').config()
 
 const express = require('express');
@@ -71,4 +44,4 @@ app.post('/product',async (req,res)=>{
 app.listen(PORT, () => {
     console.log('Server is running : listening to port ' + PORT);
 })
->>>>>>> c590f65f07a2dda7b3658fa8b006164fc6eaddd6
+
