7045c622ff5ff6c026e4e09fba0e49de11e5ff7c
dudu benh <dav_bh@outlook.com>
2022-12-12 06:42:02 +0200

    a1

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,8 +3,8 @@ const userService = require('../BL/user.service');
 const router = express.Router();
 
 
-router.post('/add', (req, res) => {
-    let user = userService.createNewUser(req.body);
+router.post('/add', async (req, res) => {
+    let user = await userService.createNewUser(req.body);
     res.send("user created");
   });
 
